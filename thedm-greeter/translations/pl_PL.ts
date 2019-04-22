115fc23d92bc5afc3a8debf4191ec87e42ef14c5
feandesign <feandesign@protonmail.com>
2019-04-22 11:59:38 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (59 of 59 strings)
    
    Translation: theDM/theDM Greeter
    Translate-URL: http://translate.vicr123.com/projects/thedm/thedm-greeter/pl/

--- thedm-greeter/translations/pl_PL.ts
+++ thedm-greeter/translations/pl_PL.ts
@@ -7,206 +7,206 @@
         <location filename="../mainwindow.ui" line="119"/>
         <location filename="../mainwindow.ui" line="429"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Wstecz</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="150"/>
         <source>Select User</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz użytkownika</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="166"/>
         <source>Power Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Opcje zasilania</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="289"/>
         <source>Who&apos;s logging in today?</source>
-        <translation type="unfinished"></translation>
+        <translation>Kto się dziś loguje?</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="350"/>
         <source>Someone Else</source>
-        <translation type="unfinished"></translation>
+        <translation>Ktoś inny</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="460"/>
         <location filename="../mainwindow.ui" line="896"/>
         <location filename="../mainwindow.ui" line="1377"/>
         <source>Log In</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaloguj się</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="559"/>
         <source>Please enter your password</source>
-        <translation type="unfinished"></translation>
+        <translation>Wprowadź swoje hasło</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="566"/>
         <source>Your mouse password cannot be used when starting a new session.</source>
-        <translation type="unfinished"></translation>
+        <translation>Twoje hasło myszy nie może zostać użyte podczas uruchamiania nowej sesji.</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="620"/>
         <source>Caps Lock is on</source>
-        <translation type="unfinished"></translation>
+        <translation>Caps Lock jest włączony</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="796"/>
         <source>Ready to log in?</source>
-        <translation type="unfinished"></translation>
+        <translation>Jesteś gotów aby się zalogować?</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="983"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Hasło</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="1002"/>
         <source>Mouse Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Hasło myszy</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="1387"/>
         <source>Start a new session or resume a paused session</source>
-        <translation type="unfinished"></translation>
+        <translation>Uruchom nową sesję lub wznów wstrzymaną sesję</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="289"/>
         <location filename="../mainwindow.cpp" line="455"/>
         <source>Hi %1!</source>
-        <translation type="unfinished"></translation>
+        <translation>Witaj %1!</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="316"/>
         <source>PAM Challenge</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyzwanie PAM</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="317"/>
         <source>Response</source>
-        <translation type="unfinished"></translation>
+        <translation>Odpowiedź</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="351"/>
         <source>PAM Message</source>
-        <translation type="unfinished"></translation>
+        <translation>Wiadomość PAM</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="415"/>
         <location filename="../mainwindow.cpp" line="563"/>
         <source>Authentication failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Uwierzytelnianie nie powiodło się.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="437"/>
         <source>PAM Account Management failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Zarządzanie kontem PAM nie powiodło się</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="440"/>
         <source>Your account has expired. Contact your system administrator for more details.</source>
-        <translation type="unfinished"></translation>
+        <translation>Twoje konto wygasło. Skontaktuj się ze swoim administratorem systemu po więcej szczegółów.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="445"/>
         <source>Unable to update your password</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie mogę zaktualizować twojego hasła</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="467"/>
         <source>PAM Credential Management failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Zarządzanie poświadczeniem PAM nie powiodło się</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="478"/>
         <source>Session unable to be opened</source>
-        <translation type="unfinished"></translation>
+        <translation>Sesja nie może zostać otwarta</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="557"/>
         <source>Couldn&apos;t log in</source>
-        <translation type="unfinished"></translation>
+        <translation>Logowanie nie powiodło się</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="770"/>
         <location filename="../mainwindow.cpp" line="850"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>Ładowanie</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="798"/>
         <location filename="../mainwindow.cpp" line="856"/>
         <source>Empty</source>
-        <translation type="unfinished"></translation>
+        <translation>Pusty</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="802"/>
         <location filename="../mainwindow.cpp" line="860"/>
         <source>Full</source>
-        <translation type="unfinished"></translation>
+        <translation>Pełny</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="807"/>
         <location filename="../mainwindow.cpp" line="863"/>
         <source>Not Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie ładuje się</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="812"/>
         <location filename="../mainwindow.cpp" line="814"/>
         <source>%1% PC Battery%2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1% Bateria komputera%2</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="819"/>
         <source>No Battery Inserted</source>
-        <translation type="unfinished"></translation>
+        <translation>Brak baterii</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="841"/>
         <source>Pair %1 using theFile to see battery status.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sparuj %1 używając theFile aby sprawdzić stan baterii.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="843"/>
         <source>%1 battery unavailable. Device trusted?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bateria %1 niedostępna. Czy to urządzenie jest zaufane?</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="847"/>
         <source>%1% battery on %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Bateria %1% w %2</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="853"/>
         <source>Discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>Rozładowywanie</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1075"/>
         <source>What&apos;s the username of the user you&apos;d like to log in as?</source>
-        <translation type="unfinished"></translation>
+        <translation>Jak nazywa się konto użytkownika, na które chcesz się zalogować?</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1076"/>
         <source>Log in as other user</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaloguj się jako inny użytkownik</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1077"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa użytkownika</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1105"/>
         <source>User not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie znaleziono użytkownika</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1106"/>
         <source>Couldn&apos;t find that user. Make sure you&apos;ve spelled the username correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nie mogę znaleźć takiego użytkownika. Upewnij się, czy nazwa użytkownika została wpisana poprawnie.</translation>
     </message>
 </context>
 <context>
@@ -214,37 +214,37 @@
     <message>
         <location filename="../pamchauthtok.ui" line="46"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Anuluj</translation>
     </message>
     <message>
         <location filename="../pamchauthtok.ui" line="74"/>
         <source>Change Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Zmień hasło</translation>
     </message>
     <message>
         <location filename="../pamchauthtok.ui" line="122"/>
         <source>You are required to change your password immediately</source>
-        <translation type="unfinished"></translation>
+        <translation>Wymagana jest natychmiastowa zmiana hasła</translation>
     </message>
     <message>
         <location filename="../pamchauthtok.ui" line="146"/>
         <source>Current Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Obecne hasło</translation>
     </message>
     <message>
         <location filename="../pamchauthtok.ui" line="156"/>
         <source>New Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Nowe hasło</translation>
     </message>
     <message>
         <location filename="../pamchauthtok.ui" line="166"/>
         <source>Confirm New Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Potwierdź nowe hasło</translation>
     </message>
     <message>
         <location filename="../pamchauthtok.ui" line="90"/>
         <source>Accept</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaakceptuj</translation>
     </message>
 </context>
 <context>
@@ -252,12 +252,12 @@
     <message>
         <location filename="../pamquestion.ui" line="46"/>
         <source>Cancel Authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Anuluj uwierzytelnianie</translation>
     </message>
     <message>
         <location filename="../pamquestion.ui" line="90"/>
         <source>Accept</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaakceptuj</translation>
     </message>
 </context>
 <context>
@@ -265,57 +265,57 @@
     <message>
         <location filename="../poweroptions.ui" line="46"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Anuluj</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="116"/>
         <source>Power Off</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyłącz</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="122"/>
         <source>Turns off your computer</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyłącza twój komputer</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="133"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Uruchom ponownie</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="139"/>
         <source>Turns off your computer and turns it back on again</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyłącza twój komputer i włącza go ponownie</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="146"/>
         <source>Suspend</source>
-        <translation type="unfinished"></translation>
+        <translation>Wstrzymaj</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="152"/>
         <source>Puts your computer in a low power state so that it opens up quickly</source>
-        <translation type="unfinished"></translation>
+        <translation>Przełącza twój komputer w stan niskiego poboru prądu, dzięki czemu szybko się uruchomi</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="159"/>
         <source>Turn Off Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyłącz ekran</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="165"/>
         <source>Turns off the screen, but keeps everything else running in the background</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyłącza ekran, ale pozostawia wszystko inne działające w tle</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="172"/>
         <source>Hibernate</source>
-        <translation type="unfinished"></translation>
+        <translation>Hibernuj</translation>
     </message>
     <message>
         <location filename="../poweroptions.ui" line="178"/>
         <source>Powers off your computer, but when powered back on, restores the session.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wyłącza twój komputer, ale po ponownym uruchomieniu wznawia sesję.</translation>
     </message>
 </context>
 </TS>
